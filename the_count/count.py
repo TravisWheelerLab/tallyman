3dd91ab1a16e4c34596b72276111b9db30be54d1
George Lesica <george@lesica.com>
2020-04-13 22:14:00 -0600

    Adjust paths, ignore files, and add lock

--- the_count/count.py
+++ the_count/count.py
@@ -3,7 +3,7 @@ import subprocess, shlex
 
 def file_len(fname):
     #run a subprocess for "grep -o '>' DCEs.fasta | wc -l" to get number of sequences in fasta file
-    command_line = "grep -o '>' ../fixtures/DCEs.fasta"
+    command_line = "grep -o '>' fixtures/DCEs.fasta"
     args = shlex.split(command_line)
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     wc = subprocess.Popen(['wc', '-l'], stdin=p.stdout, stdout=subprocess.PIPE,)
@@ -11,8 +11,8 @@ def file_len(fname):
     return int(end_of_pipe[0])
 
 if __name__ == "__main__":
-    filename = '../fixtures/DCEs.fasta'
-    RNAfile = '../fixtures/RNAseqs.fasta'
+    filename = 'fixtures/DCEs.fasta'
+    RNAfile = 'fixtures/RNAseqs.fasta'
     n = file_len(filename)
     p = 0.05  # false positive probability
     #bloomf = BloomFilter(n, p)
