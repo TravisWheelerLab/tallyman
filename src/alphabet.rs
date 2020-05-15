b84200f41f83f083421976b44c6b52d6cc4b6c04
George Lesica <george@lesica.com>
2020-05-15 11:46:32 -0600

    Add tests to alphabet and simplify demo

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -7,3 +7,27 @@ pub fn make_alphabet(nucs: &str) -> HashMap<char, u64> {
     }
     map
 }
+
+#[cfg(test)]
+mod test {
+    use crate::alphabet::make_alphabet;
+
+    #[test]
+    fn test_make_alphabet() {
+        let alpha_map = make_alphabet("ABCD");
+
+        assert_eq!(alpha_map.len(), 4);
+
+        assert!(alpha_map.contains_key(&'A'));
+        assert_eq!(alpha_map.get(&'A'), Some(&0u64));
+
+        assert!(alpha_map.contains_key(&'B'));
+        assert_eq!(alpha_map.get(&'B'), Some(&1u64));
+
+        assert!(alpha_map.contains_key(&'C'));
+        assert_eq!(alpha_map.get(&'C'), Some(&2u64));
+
+        assert!(alpha_map.contains_key(&'D'));
+        assert_eq!(alpha_map.get(&'D'), Some(&3u64));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
-use std::path::Path;
-
 use crate::fasta::SeqLoader;
 use crate::search::Search;
-use std::io::{stdout, Write};
+use std::path::Path;
 
 pub mod alphabet;
 pub mod compress;
 pub mod fasta;
+pub mod parallel;
 pub mod search;
 
 fn main() {
-    let haystacks = SeqLoader::from_path(Path::new("experiments/SRR1099957.fasta"));
-    let needles: Vec<_> =
-        SeqLoader::from_path(Path::new("experiments/dce-sequences.fasta")).collect();
-
-    let mut done = 0;
+    let haystacks = SeqLoader::from_path(Path::new("testRNA.fasta"));
+    let needles: Vec<_> = SeqLoader::from_path(Path::new("fixtures/DCEs.fasta")).collect();
+    let alphabet = "ATCG";
     for haystack in haystacks {
-        for result in Search::new(&haystack, &needles, "ATCG") {
-            eprint!("\r");
-            println!("{}", result.haystack);
-        }
-
-        done += 1;
-
-        if done % 100 == 0 {
-            eprint!("\r");
-            eprint!("{} of 45409706 completed", done);
-            stdout().flush();
+        for result in Search::new(&haystack, &needles, &alphabet) {
+            println!(
+                "{} found in {} at offset {}",
+                result.needle, result.haystack, result.offset
+            );
         }
     }
 }
