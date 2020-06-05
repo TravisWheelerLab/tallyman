027108dec501f9645a69b6790c586ba4e098c1fa
George Lesica <george@lesica.com>
2020-06-04 21:29:38 -0600

    Support lowercase letters

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,10 +1,26 @@
 pub const ALPHABET: [char; 4] = ['A', 'C', 'G', 'T'];
 
-// TODO: Handle lowercase letters
 #[inline]
 pub fn encode_char(c: char) -> u64 {
-    match ALPHABET.binary_search(&c) {
-        Ok(i) => i as u64,
-        Err(_) => panic!(format!("invalid character '{}'", c)),
+    match c {
+        'A' | 'a' => 0,
+        'C' | 'c' => 1,
+        'G' | 'g' => 2,
+        'T' | 't' => 3,
+        _ => panic!(format!("invalid character '{}'", c)),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::alphabet::encode_char;
+
+    #[test]
+    fn test_encode_char() {
+        let low_a = encode_char('a');
+        assert_eq!(low_a, 0);
+
+        let cap_a = encode_char('A');
+        assert_eq!(cap_a, 0);
     }
 }
