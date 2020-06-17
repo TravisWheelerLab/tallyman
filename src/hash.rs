eea072f6c8c3ed6c36d4538a27e94ffb392853f0
George Lesica <george@lesica.com>
2020-06-17 16:51:24 -0600

    Simplify hash to require less memory

--- src/hash.rs
+++ src/hash.rs
@@ -3,24 +3,20 @@
 /// to be extremely lightweight to improve performance.
 pub struct Hash {
     container: Vec<u64>,
-    occupied: Vec<usize>,
-    capacity: usize,
-    size: usize,
+    capacity: u64,
 }
 
 impl Hash {
     pub fn new(capacity: usize) -> Hash {
         Hash {
             container: vec![0; capacity],
-            occupied: vec![0; capacity],
-            capacity,
-            size: 0,
+            capacity: capacity as u64,
         }
     }
 
     /// Add the given value to the set.
     pub fn add(&mut self, value: u64) {
-        let hv = value % (self.capacity as u64);
+        let hv = value % self.capacity;
 
         // We may now cast hv to a usize because we're sure
         // that it is < self.size and will therefore fit.
@@ -28,10 +24,10 @@ impl Hash {
         let mut probed_index = hv_index;
 
         // Find the next empty slot (this is the linear probing bit).
-        while self.occupied[probed_index] != 0 {
+        while self.container[probed_index] != 0 {
             probed_index += 1;
 
-            if probed_index >= self.capacity {
+            if probed_index >= self.capacity as usize {
                 probed_index = 0;
             }
 
@@ -40,37 +36,35 @@ impl Hash {
             }
         }
 
-        self.size += 1;
         self.container[probed_index] = value;
-        self.occupied[probed_index] = self.size;
     }
 
     /// Return the position in the insertion order for the
     /// given value, or `0` if the value is not present.
-    pub fn contains(&self, value: u64) -> usize {
-        let hv = value % (self.capacity as u64);
+    pub fn contains(&self, value: u64) -> bool {
+        let hv = value % self.capacity;
 
         // We may now cast hv to a usize because we're sure
         // that it is < self.size and will therefore fit.
         let hv_index = hv as usize;
         let mut probed_index = hv_index;
 
-        while self.occupied[probed_index] != 0 {
+        while self.container[probed_index] != 0 {
             if self.container[probed_index] == value {
-                return self.occupied[probed_index];
+                return true;
             }
             probed_index += 1;
 
-            if probed_index >= self.capacity {
+            if probed_index >= self.capacity as usize {
                 probed_index = 0;
             }
 
             if probed_index == hv_index {
-                return 0;
+                return false;
             }
         }
 
-        0
+        false
     }
 }
 
@@ -81,41 +75,36 @@ mod test {
     #[test]
     fn test_create_hash() {
         let hash = Hash::new(10);
+        assert_eq!(hash.capacity, 10);
+        assert_eq!(hash.container.len(), 10);
         for i in 0..10 {
             assert_eq!(hash.container[i], 0);
-            assert_eq!(hash.occupied[i], 0);
         }
     }
 
     #[test]
     fn test_add_to_hash() {
         let mut hash = Hash::new(10);
-        hash.add(0);
-        hash.add(1);
+        hash.add(10);
+        hash.add(11);
 
-        assert_eq!(hash.container[0], 0);
-        assert_eq!(hash.occupied[0], 1);
-
-        assert_eq!(hash.container[1], 1);
-        assert_eq!(hash.occupied[1], 2);
+        assert_eq!(hash.container[0], 10);
+        assert_eq!(hash.container[1], 11);
     }
 
     #[test]
     fn test_contains_value() {
         let mut hash = Hash::new(10);
-        hash.occupied[0] = 1;
-        hash.container[0] = 0;
-        hash.occupied[2] = 2;
-        hash.container[2] = 2;
+        hash.container[0] = 10;
+        hash.container[2] = 12;
         // Collision that had to probe
-        hash.occupied[3] = 3;
-        hash.container[3] = 12;
+        hash.container[3] = 2;
 
-        assert_eq!(hash.contains(0), 1);
-        assert_eq!(hash.contains(2), 2);
-        assert_eq!(hash.contains(12), 3);
+        assert_eq!(hash.contains(10), true);
+        assert_eq!(hash.contains(12), true);
+        assert_eq!(hash.contains(2), true);
 
-        assert_eq!(hash.contains(1), 0);
-        assert_eq!(hash.contains(13), 0);
+        assert_eq!(hash.contains(1), false);
+        assert_eq!(hash.contains(11), false);
     }
 }
