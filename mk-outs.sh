9d59b1138cc8094ca13f031a6fbba705e3251b21
Ken Youens-Clark <kyclark@gmail.com>
2024-06-25 14:03:04 -0700

    new stuff

---
+++ mk-outs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -u
+
+PRG="cargo run --"
+DNA_FA="tests/inputs/dna.fasta"
+
+RNA_FA_50K="tests/inputs/rna-50k.fasta"
+RNA_FA_100K="tests/inputs/rna-100k.fasta"
+
+RNA_FQ_50K="tests/inputs/rna-50k.fastq"
+RNA_FQ_100K="tests/inputs/rna-100k.fastq"
+
+OUT_FA_50K="tests/outputs/out-50k-fasta.txt"
+OUT_FA_100K="tests/outputs/out-100k-fasta.txt"
+
+OUT_FQ_50K="tests/outputs/out-50k-fastq.txt"
+OUT_FQ_100K="tests/outputs/out-100k-fastq.txt"
+
+$PRG -d $DNA_FA -r $RNA_FA_50K  -o $OUT_FA_50K
+$PRG -d $DNA_FA -r $RNA_FA_100K -o $OUT_FA_100K
+
+$PRG -d $DNA_FA -r $RNA_FQ_50K  -o $OUT_FQ_50K
+$PRG -d $DNA_FA -r $RNA_FQ_100K -o $OUT_FQ_100K
